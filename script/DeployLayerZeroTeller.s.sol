1aadcfd9857538aa7dbb566c87f0dae750f18e52
crispymangoes <crispymangoes@gmail.com>
2025-03-10 11:43:58 -0700

    Deploy liquid btc

--- script/DeployLayerZeroTeller.s.sol
+++ script/DeployLayerZeroTeller.s.sol
@@ -23,27 +23,60 @@ contract DeployLayerZeroTellerScript is Script, ContractNames {
     LayerZeroTellerWithRateLimiting public layerZeroTeller;
     address internal deployerAddress = 0x5F2F11ad8656439d5C14d9B351f8b09cDaC2A02d;
     address internal dev1Address = 0xf8553c8552f906C19286F21711721E206EE4909E;
-    address internal weth = 0x5300000000000000000000000000000000000004;
-    address internal boringVault = 0x657e8C867D8B37dCC18fA4Caead9C45EB088C642;
-    address internal accountant = 0x1b293DC39F94157fA0D1D36d7e0090C8B8B8c13F;
+    address internal weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address internal lzEndPoint = 0x1a44076050125825900e736c501f859c50fE728c;
     address internal delegate = dev1Address; // I do not think we need this functionality, but for future use, setDelegate has a requires auth modifier so it can be changed.
 
     function setUp() external {
         privateKey = vm.envUint("BORING_DEVELOPER");
-        vm.createSelectFork("scroll");
+        vm.createSelectFork("mainnet");
     }
 
     function run() external {
         bytes memory creationCode;
         bytes memory constructorArgs;
+        address boringVault;
+        address accountant;
         vm.startBroadcast(privateKey);
 
+        // // eBTC
+        // boringVault = 0x657e8C867D8B37dCC18fA4Caead9C45EB088C642;
+        // accountant = 0x1b293DC39F94157fA0D1D36d7e0090C8B8B8c13F;
+        // deployer = Deployer(deployerAddress);
+        // creationCode = type(LayerZeroTellerWithRateLimiting).creationCode;
+        // constructorArgs = abi.encode(dev1Address, boringVault, accountant, weth, lzEndPoint, delegate, address(0));
+        // layerZeroTeller = LayerZeroTellerWithRateLimiting(
+        //     deployer.deployContract("eBTC LayerZero Teller V0.0", creationCode, constructorArgs, 0)
+        // );
+
+        // eUSD
+        boringVault = 0x939778D83b46B456224A33Fb59630B11DEC56663;
+        accountant = 0xEB440B36f61Bf62E0C54C622944545f159C3B790;
         deployer = Deployer(deployerAddress);
         creationCode = type(LayerZeroTellerWithRateLimiting).creationCode;
         constructorArgs = abi.encode(dev1Address, boringVault, accountant, weth, lzEndPoint, delegate, address(0));
         layerZeroTeller = LayerZeroTellerWithRateLimiting(
-            deployer.deployContract("eBTC LayerZero Teller V0.0", creationCode, constructorArgs, 0)
+            deployer.deployContract("eUSD LayerZero Teller V0.0", creationCode, constructorArgs, 0)
+        );
+
+        // LiquidUSD
+        boringVault = 0x08c6F91e2B681FaF5e17227F2a44C307b3C1364C;
+        accountant = 0xc315D6e14DDCDC7407784e2Caf815d131Bc1D3E7;
+        deployer = Deployer(deployerAddress);
+        creationCode = type(LayerZeroTellerWithRateLimiting).creationCode;
+        constructorArgs = abi.encode(dev1Address, boringVault, accountant, weth, lzEndPoint, delegate, address(0));
+        layerZeroTeller = LayerZeroTellerWithRateLimiting(
+            deployer.deployContract("LiquidUSD LayerZero Teller V0.0", creationCode, constructorArgs, 0)
+        );
+
+        // LiquidETH
+        boringVault = 0xf0bb20865277aBd641a307eCe5Ee04E79073416C;
+        accountant = 0x0d05D94a5F1E76C18fbeB7A13d17C8a314088198;
+        deployer = Deployer(deployerAddress);
+        creationCode = type(LayerZeroTellerWithRateLimiting).creationCode;
+        constructorArgs = abi.encode(dev1Address, boringVault, accountant, weth, lzEndPoint, delegate, address(0));
+        layerZeroTeller = LayerZeroTellerWithRateLimiting(
+            deployer.deployContract("LiquidETH LayerZero Teller V0.0", creationCode, constructorArgs, 0)
         );
 
         vm.stopBroadcast();
