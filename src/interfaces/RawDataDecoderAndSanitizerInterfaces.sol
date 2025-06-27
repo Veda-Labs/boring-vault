48b983272991d76a698e379ae7a85d941ebc3a40
Xiangyu Xu <xiangyuxu329@gmail.com>
2025-06-26 23:36:54 -0700

    added Shadow's INonfungiblePositionManager

--- src/interfaces/RawDataDecoderAndSanitizerInterfaces.sol
+++ src/interfaces/RawDataDecoderAndSanitizerInterfaces.sol
@@ -45,6 +45,45 @@ interface INonFungiblePositionManager {
         );
 }
 
+//@notice Shadow Exchange's Centralized Liquidity Pool has customized some of the configs,
+//    hence the need to have a separate interface for it. For example, no permitting functionality
+//    so no nonce and no operator; and PoolKey is determined by token0, token1, and tickSpacing instead
+//    of fee.
+interface INonFungiblePositionManagerShadow {
+    struct Position {
+        // the ID of the pool with which this token is connected
+        uint80 poolId;
+        // the tick range of the position
+        int24 tickLower;
+        int24 tickUpper;
+        // the liquidity of the position
+        uint128 liquidity;
+        // the fee growth of the aggregate position as of the last action on the individual position
+        uint256 feeGrowthInside0LastX128;
+        uint256 feeGrowthInside1LastX128;
+        // how many uncollected tokens are owed to the position, as of the last computation
+        uint128 tokensOwed0;
+        uint128 tokensOwed1;
+    }
+
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function positions(uint256 tokenId)
+        external
+        view
+        returns (
+            address token0,
+            address token1,
+            int24 tickSpacing,
+            int24 tickLower,
+            int24 tickUpper,
+            uint128 liquidity,
+            uint256 feeGrowthInside0LastX128,
+            uint256 feeGrowthInside1LastX128,
+            uint128 tokensOwed0,
+            uint128 tokensOwed1
+        );
+}
+
 interface PancakeSwapV3MasterChef {
     function userPositionInfos(uint256 id)
         external
