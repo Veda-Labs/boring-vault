8053818a805e69bae5709ae70fcee6e27483dd7a
mel0ndev <motherfker.jones1@gmail.com>
2025-07-14 15:17:32 -0400

    deployed

--- script/DeployGenericRateProviderWithDecimalScaling.s.sol
+++ script/DeployGenericRateProviderWithDecimalScaling.s.sol
@@ -18,13 +18,13 @@ import "forge-std/Test.sol";
 contract DeployGenericRateProviderWithDecimalScaling is Script, ContractNames, Test {
     uint256 public privateKey;
     
-    address target = 0x3Eae75C0a2f9b1038C7c9993C1Da36281E838811; 
+    address target = 0xb9D0073aCb296719C26a8BF156e4b599174fe1d5; 
     bytes4 selector = 0x50d25bcd; 
     Deployer deployer = Deployer(0x5F2F11ad8656439d5C14d9B351f8b09cDaC2A02d); 
 
     function setUp() external {
         privateKey = vm.envUint("BORING_DEVELOPER");
-        vm.createSelectFork("katana");
+        vm.createSelectFork("tac");
     }
 
     function run() external {
@@ -44,8 +44,7 @@ contract DeployGenericRateProviderWithDecimalScaling is Script, ContractNames, T
             8,
             18
         ));
-        address createdAddress = deployer.deployContract("WeETH Rate Provider V0.0", creationCode, constructorArgs, 0); 
+        address createdAddress = deployer.deployContract("tsTON Rate Provider V0.0", creationCode, constructorArgs, 0); 
         console.log("DEPLOYED ADDRESS: ", createdAddress); 
     }
-
 }
