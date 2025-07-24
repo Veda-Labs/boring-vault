540c2638a63a0ffd85a50e19b687e8bf06044962
mel0ndev <motherfker.jones1@gmail.com>
2025-07-24 14:40:02 -0400

    tac btc

--- script/DeployDecoderAndSanitizer.s.sol
+++ script/DeployDecoderAndSanitizer.s.sol
@@ -118,9 +118,9 @@ contract DeployDecoderAndSanitizerScript is Script, ContractNames, MainnetAddres
         //constructorArgs = abi.encode(getAddress(sourceChain, "uniswapV3NonFungiblePositionManager"), getAddress(sourceChain, "convexFXPoolRegistry"), getAddress(sourceChain, "odosRouterV2"));
         //deployer.deployContract("Lombard BTC Decoder And Sanitizer v0.5", creationCode, constructorArgs, 0);
 
-        creationCode = type(KatanaDecoderAndSanitizer).creationCode;
-        constructorArgs = abi.encode(getAddress(sourceChain, "uniswapV3NonFungiblePositionManager"));
-        deployer.deployContract("Katana Decoder And Sanitizer V0.6", creationCode, constructorArgs, 0);
+        //creationCode = type().creationCode;
+        //constructorArgs = abi.encode(getAddress(sourceChain, "uniswapV3NonFungiblePositionManager"));
+        //deployer.deployContract("Katana Decoder And Sanitizer V0.6", creationCode, constructorArgs, 0);
 
         //address uniswapV4PositionManager = getAddress(sourceChain, "uniV4PositionManager");
         //address uniswapV3NonFungiblePositionManager = getAddress(sourceChain, "uniswapV3NonFungiblePositionManager");
@@ -131,10 +131,9 @@ contract DeployDecoderAndSanitizerScript is Script, ContractNames, MainnetAddres
         //    abi.encode(uniswapV4PositionManager, uniswapV3NonFungiblePositionManager, odosRouterV2, dvStETHVault);
         //deployer.deployContract("Golden Goose Decoder And Sanitizer v0.4", creationCode, constructorArgs, 0);
 
-        creationCode = type(TacTONDecoderAndSanitizer).creationCode;
+        creationCode = type(TacDecoderAndSanitizer).creationCode;
         constructorArgs = abi.encode();
-        deployer.deployContract("TAC Decoder And Sanitizer v0.0", creationCode, constructorArgs, 0);
-
+        deployer.deployContract("TAC Decoder And Sanitizer v0.1", creationCode, constructorArgs, 0);
 
         vm.stopBroadcast();
     }
