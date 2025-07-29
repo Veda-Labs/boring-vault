e625e8c27f944c4fa594426fe24aa243d5caf813
mel0ndev <motherfker.jones1@gmail.com>
2025-07-29 10:50:47 -0400

    rm usused

--- src/base/DecodersAndSanitizers/Protocols/ValantisDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/Protocols/ValantisDecoderAndSanitizer.sol
@@ -6,24 +6,6 @@ import {DecoderCustomTypes} from "src/interfaces/DecoderCustomTypes.sol";
 contract ValantisDecoderAndSanitizer {
     //============================== ERRORS ===============================
     error ValantisDecoderAndSanitizer__PoolsLengthGtOne(); 
-
-    //function swap(DecoderCustomTypes.DirectSwapParams calldata _directSwapParams) external virtual returns (bytes memory addressesFound) {
-    //    //limit swaps to the LSTs (kHYPE/WHYPE)
-    //    if (_directSwapParams.pools.length > 1) revert ValantisDecoderAndSanitizer__PoolsLengthGtOne(); 
-    //   
-    //    address payloadRecipient = _directSwapParams.isUniversalPool == true ? 
-    //        _decodeUniversalPayload(_directSwapParams.payload) : 
-    //        _decodeSovereignPayload(_directSwapParams.payload)
-    //    ;  
-    //    
-    //    addressesFound = abi.encodePacked(
-    //        _directSwapParams.pools[0],  
-    //        payloadRecipient,
-    //        _directSwapParams.tokenIn,
-    //        _directSwapParams.tokenOut,
-    //        _directSwapParams.recipient
-    //    ); 
-    //}
     
     // @dev sov pool
     function swap(DecoderCustomTypes.SovereignPoolSwapParams calldata _swapParams) external virtual returns (bytes memory addressesFound) {
@@ -55,16 +37,4 @@ contract ValantisDecoderAndSanitizer {
     function claim(uint256 /*_idLPQueue*/) external virtual returns (bytes memory addressesFound) {
         return addressesFound; 
     }
-
-    //function _decodeUniversalPayload(bytes payload) internal view returns (address) {
-    //    DecoderCustomTypes.UniversalPoolSwapPayload memory payloadStruct = abi.decode(payload, (DecoderCustomTypes.UniversalPoolSwapPayload));
-    //    return payloadStruct.recipient; 
-    //}
-
-    //function _decodeSovereignPayload(bytes payload) internal view returns (address) {
-    //    DecoderCustomTypes.SovereignPoolSwapPayload memory payloadStruct = abi.decode(payload, (DecoderCustomTypes.SovereignPoolSwapPayload));
-    //    return payloadStruct.recipient; 
-    //}
 }
-
-
