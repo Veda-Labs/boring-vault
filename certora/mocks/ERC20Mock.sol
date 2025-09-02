7435c323eab26fb4fb461a29e957d1cdf642e15c
Gereon Kremer <gereon.kremer@gmail.com>
2025-09-02 14:42:15 +0200

    rewrite transfer and transferFrom to not allow for overflows

--- certora/mocks/ERC20Mock.sol
+++ certora/mocks/ERC20Mock.sol
@@ -9,4 +9,31 @@ contract ERC20Mock is ERC20 {
         string memory _symbol,
         uint8 _decimals
     ) ERC20(_name, _symbol, _decimals) {}
+
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
+
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
+        uint256 allowed = allowance[from][msg.sender]; // Saves gas for limited approvals.
+
+        if (allowed != type(uint256).max) allowance[from][msg.sender] = allowed - amount;
+
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+
+        emit Transfer(from, to, amount);
+
+        return true;
+    }
 }
