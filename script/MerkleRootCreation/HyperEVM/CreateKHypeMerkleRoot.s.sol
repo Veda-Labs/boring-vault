d85f2e46ee3fbfb00d82dcd19f288da64740bad2
mel0ndev <motherfker.jones1@gmail.com>
2025-09-04 14:50:24 -0400

    add project x

--- script/MerkleRootCreation/HyperEVM/CreateKHypeMerkleRoot.s.sol
+++ script/MerkleRootCreation/HyperEVM/CreateKHypeMerkleRoot.s.sol
@@ -19,7 +19,7 @@ contract CreateKHypeMerkleRoot is Script, MerkleTreeHelper {
 
     //standard
     address public boringVault = 0x9BA2EDc44E0A4632EB4723E81d4142353e1bB160;
-    address public rawDataDecoderAndSanitizer = 0x0C95a35f25160B46dF33589596bdE92848Eb6Df8;
+    address public rawDataDecoderAndSanitizer = 0x0Ac1819A5EA6cAf05306b8955bC1a1680fA7B63A;
     address public managerAddress = 0x7f8CcAA760E0F621c7245d47DC46d40A400d3639;
     address public accountantAddress = 0x74392Fa56405081d5C7D93882856c245387Cece2;
 
@@ -83,6 +83,15 @@ contract CreateKHypeMerkleRoot is Script, MerkleTreeHelper {
 
         // ========================== Valantis ==========================
         _addValantisLSTLeafs(leafs, getAddress(sourceChain, "KHYPE_WHYPE_sovereign_pool"), false); 
+
+        // ========================== UniswapV3/Project X ==========================
+        address[] memory token0 = new address[](1);
+        token0[0] = getAddress(sourceChain, "KHYPE");
+
+        address[] memory token1 = new address[](1);
+        token1[0] = getAddress(sourceChain, "WHYPE");
+
+        _addUniswapV3Leafs(leafs, token0, token1, false);
 
         // ========================== Verify ==========================
         _verifyDecoderImplementsLeafsFunctionSelectors(leafs);
