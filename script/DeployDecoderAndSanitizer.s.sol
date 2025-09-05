88d9ebb7b939ebcb039c3e711d2982cdbf1de21d
oxGato-Veda <ryan@veda.tech>
2025-09-05 14:02:48 -0400

    Add MF-One to merkle root, redeploy decoder

--- script/DeployDecoderAndSanitizer.s.sol
+++ script/DeployDecoderAndSanitizer.s.sol
@@ -123,9 +123,7 @@ contract DeployDecoderAndSanitizerScript is Script, ContractNames, MainnetAddres
 
     function setUp() external {
         privateKey = vm.envUint("BORING_DEVELOPER");
-        
-        vm.createSelectFork("katana");
-        setSourceChainName("katana");
+    
 
         vm.createSelectFork("mainnet");
         setSourceChainName("mainnet"); 
@@ -139,72 +137,73 @@ contract DeployDecoderAndSanitizerScript is Script, ContractNames, MainnetAddres
         vm.startBroadcast(privateKey);
 
 
-        creationCode = type(LombardBtcDecoderAndSanitizer).creationCode;
-        addressKeys = ["uniswapV3NonFungiblePositionManager", "convexFXPoolRegistry", "odosRouterV2"];
-        deployContract("Lombard BTC Decoder And Sanitizer v0.5", creationCode, 0);
+        // creationCode = type(LombardBtcDecoderAndSanitizer).creationCode;
+        // addressKeys = ["uniswapV3NonFungiblePositionManager", "convexFXPoolRegistry", "odosRouterV2"];
+        // deployContract("Lombard BTC Decoder And Sanitizer v0.5", creationCode, 0);
 
-        creationCode = type(KatanaDecoderAndSanitizer).creationCode;
-        addressKeys = ["uniswapV3NonFungiblePositionManager"];
-        deployContract("Katana Decoder And Sanitizer V0.8", creationCode, 0);
+        // creationCode = type(KatanaDecoderAndSanitizer).creationCode;
+        // addressKeys = ["uniswapV3NonFungiblePositionManager"];
+        // deployContract("Katana Decoder And Sanitizer V0.8", creationCode, 0);
 
-        creationCode = type(GoldenGooseDecoderAndSanitizer).creationCode;
-        addressKeys = ["uniV4PositionManager", "uniswapV3NonFungiblePositionManager", "odosRouterV2", "dvStETHVault"];
-        deployContract("Golden Goose Decoder And Sanitizer v0.4", creationCode, 0);
+        // creationCode = type(GoldenGooseDecoderAndSanitizer).creationCode;
+        // addressKeys = ["uniV4PositionManager", "uniswapV3NonFungiblePositionManager", "odosRouterV2", "dvStETHVault"];
+        // deployContract("Golden Goose Decoder And Sanitizer v0.4", creationCode, 0);
 
-        // Deploy ConvexFXDecoderAndSanitizer
-        creationCode = type(ConvexFXDecoderAndSanitizer).creationCode;
-        addressKeys = ["convexFXPoolRegistry"];
-        deployContract("Convex FX Decoder and Sanitizer V0.0", creationCode, 0);
+        // // Deploy ConvexFXDecoderAndSanitizer
+        // creationCode = type(ConvexFXDecoderAndSanitizer).creationCode;
+        // addressKeys = ["convexFXPoolRegistry"];
+        // deployContract("Convex FX Decoder and Sanitizer V0.0", creationCode, 0);
 
-        // Deploy DolomiteDecoderAndSanitizer
-        creationCode = type(DolomiteDecoderAndSanitizer).creationCode;
-        addressKeys = ["dolomiteMargin"];
-        deployContract("Dolomite Decoder and Sanitizer V0.0", creationCode, 0);
+        // // Deploy DolomiteDecoderAndSanitizer
+        // creationCode = type(DolomiteDecoderAndSanitizer).creationCode;
+        // addressKeys = ["dolomiteMargin"];
+        // deployContract("Dolomite Decoder and Sanitizer V0.0", creationCode, 0);
 
-        // Deploy DvStETHDecoderAndSanitizer
-        creationCode = type(DvStETHDecoderAndSanitizer).creationCode;
-        addressKeys = ["dvStETHVault"];
-        deployContract("Dv St ETH Decoder and Sanitizer V0.1", creationCode, 0);
+        // // Deploy DvStETHDecoderAndSanitizer
+        // creationCode = type(DvStETHDecoderAndSanitizer).creationCode;
+        // addressKeys = ["dvStETHVault"];
+        // deployContract("Dv St ETH Decoder and Sanitizer V0.1", creationCode, 0);
 
-        // Deploy OdosDecoderAndSanitizer
-        creationCode = type(OdosDecoderAndSanitizer).creationCode;
-        addressKeys = ["odosRouterV2"];
-        deployContract("Odos Decoder and Sanitizer V0.0", creationCode, 0);
+        // // Deploy OdosDecoderAndSanitizer
+        // creationCode = type(OdosDecoderAndSanitizer).creationCode;
+        // addressKeys = ["odosRouterV2"];
+        // deployContract("Odos Decoder and Sanitizer V0.0", creationCode, 0);
 
-        // Deploy PancakeSwapV3DecoderAndSanitizer
-        creationCode = type(PancakeSwapV3DecoderAndSanitizer).creationCode;
-        addressKeys = ["pancakeSwapV3NonFungiblePositionManager", "pancakeSwapV3MasterChefV3"];
-        deployContract("Pancake Swap V3 Decoder and Sanitizer V0.0", creationCode, 0);
+        // // Deploy PancakeSwapV3DecoderAndSanitizer
+        // creationCode = type(PancakeSwapV3DecoderAndSanitizer).creationCode;
+        // addressKeys = ["pancakeSwapV3NonFungiblePositionManager", "pancakeSwapV3MasterChefV3"];
+        // deployContract("Pancake Swap V3 Decoder and Sanitizer V0.0", creationCode, 0);
 
-        // Deploy RoycoWeirollDecoderAndSanitizer
-        creationCode = type(RoycoWeirollDecoderAndSanitizer).creationCode;
-        addressKeys = ["recipeMarketHub"];
-        deployContract("Royco Decoder and Sanitizer V0.1", creationCode, 0);
+        // // Deploy RoycoWeirollDecoderAndSanitizer
+        // creationCode = type(RoycoWeirollDecoderAndSanitizer).creationCode;
+        // addressKeys = ["recipeMarketHub"];
+        // deployContract("Royco Decoder and Sanitizer V0.1", creationCode, 0);
 
-        // Deploy UniswapV3DecoderAndSanitizer
-        creationCode = type(UniswapV3DecoderAndSanitizer).creationCode;
-        addressKeys = ["uniswapV3NonFungiblePositionManager"];
-        deployContract("Uniswap V3 Decoder and Sanitizer V0.0", creationCode, 0);
+        // // Deploy UniswapV3DecoderAndSanitizer
+        // creationCode = type(UniswapV3DecoderAndSanitizer).creationCode;
+        // addressKeys = ["uniswapV3NonFungiblePositionManager"];
+        // deployContract("Uniswap V3 Decoder and Sanitizer V0.0", creationCode, 0);
 
-        // Deploy UniswapV3SwapRouter02DecoderAndSanitizer
-        creationCode = type(UniswapV3SwapRouter02DecoderAndSanitizer).creationCode;
-        addressKeys = ["uniswapV3NonFungiblePositionManager"];
-        deployContract("Uniswap V3 Swap Router02 Decoder and Sanitizer V0.1", creationCode, 0);
+        // // Deploy UniswapV3SwapRouter02DecoderAndSanitizer
+        // creationCode = type(UniswapV3SwapRouter02DecoderAndSanitizer).creationCode;
+        // addressKeys = ["uniswapV3NonFungiblePositionManager"];
+        // deployContract("Uniswap V3 Swap Router02 Decoder and Sanitizer V0.1", creationCode, 0);
 
-        // Deploy UniswapV4DecoderAndSanitizer
-        creationCode = type(UniswapV4DecoderAndSanitizer).creationCode;
-        addressKeys = ["uniV4PositionManager"];
-        deployContract("Uniswap V4 Decoder and Sanitizer V0.1", creationCode, 0);
+        // // Deploy UniswapV4DecoderAndSanitizer
+        // creationCode = type(UniswapV4DecoderAndSanitizer).creationCode;
+        // addressKeys = ["uniV4PositionManager"];
+        // deployContract("Uniswap V4 Decoder and Sanitizer V0.1", creationCode, 0);
 
-        // Deploy VelodromeDecoderAndSanitizer
-        creationCode = type(VelodromeDecoderAndSanitizer).creationCode;
-        addressKeys = ["velodromeNonFungiblePositionManager"];
-        deployContract("Velodrome Decoder and Sanitizer V0.0", creationCode, 0);
+        // // Deploy VelodromeDecoderAndSanitizer
+        // creationCode = type(VelodromeDecoderAndSanitizer).creationCode;
+        // addressKeys = ["velodromeNonFungiblePositionManager"];
+        // deployContract("Velodrome Decoder and Sanitizer V0.0", creationCode, 0);
 
         // Deploy AtomicQueueDecoderAndSanitizer
-        creationCode = type(AtomicQueueDecoderAndSanitizer).creationCode;
-        constructorArgs = abi.encode(0.9e4, 1.1e4);
-        deployer.deployContract("Atomic Queue Decoder and Sanitizer V0.0", creationCode, constructorArgs, 0);
+        creationCode = type(EtherFiLiquidUsdDecoderAndSanitizer).creationCode;
+        constructorArgs = abi.encode(getAddress(sourceChain, "uniswapV3NonFungiblePositionManager"), getAddress(sourceChain, "odosRouterV2"));
+        console.logBytes(constructorArgs);
+        deployer.deployContract("EtherFi Liquid USD Decoder and Sanitizer V1.1", creationCode, constructorArgs, 0);
 
         //creationCode = type(TacDecoderAndSanitizer).creationCode;
         //creationCode = type(LombardBtcDecoderAndSanitizer).creationCode;
