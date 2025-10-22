94dea23ab7c2025b5f677f73bf5b799817fcf9ee
oxGato-Veda <ryan@veda.tech>
2025-10-22 14:40:26 -0400

    update super symbiotic strategist leafs

--- script/DeployDecoderAndSanitizer.s.sol
+++ script/DeployDecoderAndSanitizer.s.sol
@@ -136,8 +136,8 @@ contract DeployDecoderAndSanitizerScript is Script, ContractNames, MainnetAddres
     function setUp() external {
         privateKey = vm.envUint("BORING_DEVELOPER");
 
-        vm.createSelectFork("bsc");
-        setSourceChainName("bsc");
+        vm.createSelectFork("mainnet");
+        setSourceChainName("mainnet");
     }
 
     function run() external {
@@ -145,13 +145,13 @@ contract DeployDecoderAndSanitizerScript is Script, ContractNames, MainnetAddres
         bytes memory constructorArgs;
         vm.startBroadcast(privateKey);
 
-        creationCode = type(TurtleMUSDDecoderAndSanitizer).creationCode;
-        constructorArgs = abi.encode(getAddress(sourceChain, "odosRouterV2"), getAddress(sourceChain, "pancakeSwapV3NonFungiblePositionManager"), getAddress(sourceChain, "pancakeSwapV3MasterChefV3"));
-        deployer.deployContract("turtlemUSD Decoder and Sanitizer V0.0", creationCode, constructorArgs, 0);
+        // creationCode = type(TurtleMUSDDecoderAndSanitizer).creationCode;
+        // constructorArgs = abi.encode(getAddress(sourceChain, "odosRouterV2"), getAddress(sourceChain, "pancakeSwapV3NonFungiblePositionManager"), getAddress(sourceChain, "pancakeSwapV3MasterChefV3"));
+        // deployer.deployContract("turtlemUSD Decoder and Sanitizer V0.0", creationCode, constructorArgs, 0);
 
-        creationCode = type(SentoraDecoderAndSanitizer).creationCode;
-        constructorArgs = abi.encode(getAddress(sourceChain, "odosRouterV2"));
-        deployer.deployContract("Sentora Decoder and Sanitizer V0.2", creationCode, constructorArgs, 0);
+        creationCode = type(SymbioticLRTDecoderAndSanitizer).creationCode;
+        constructorArgs = abi.encode(getAddress(sourceChain, "uniswapV3PositionManager"), getAddress(sourceChain, "odosRouterV2"));
+        deployer.deployContract("Symbiotic LRT Decoder and Sanitizer V0.1", creationCode, constructorArgs, 0);
 
         vm.stopBroadcast();
     }
