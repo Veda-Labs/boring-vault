56c3558e6f7c97909dda0bb018fd171a9d2e09d9
Eidolon <92181746+imrtlfarm@users.noreply.github.com>
2025-10-27 09:22:49 -0700

    TestVault1 deployed

--- test/resources/ChainValues.sol
+++ test/resources/ChainValues.sol
@@ -2306,7 +2306,7 @@ contract ChainValues {
 
         values[sepolia]["WETH"] = 0xb16F35c0Ae2912430DAc15764477E179D9B9EbEa.toBytes32();
         values[sepolia]["CrispyUSD"] = 0x867F14Da2EcD4B582812d76D94c4B10cB00b507C.toBytes32();
-        values[sepolia]["USDC"] = 0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8.toBytes32(); //AAVEv3 USDC
+        values[sepolia]["USDC"] = 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238.toBytes32(); //AAVEv3 USDC
         values[sepolia]["ZRO"] = address(1).toBytes32();
         values[sepolia]["CrispyCoin"] = 0x0c959E3AA0A74E972d1A8F759c198e660CcCebcB.toBytes32();
         values[sepolia]["WETH9"] = 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14.toBytes32();
@@ -2866,6 +2866,8 @@ contract ChainValues {
         values[inkSepolia]["WETH"] = 0x4200000000000000000000000000000000000006.toBytes32(); //Sepolia WETH
 
         values[inkSepolia]["balancerVault"] = address(1).toBytes32();
+
+        values[inkSepolia]["LayerZeroEndPoint"] = 0x6C7Ab2202C98C4227C5c46f1417D81144DA716Ff.toBytes32();
     }
 
     function _addPlumeValues() private {
