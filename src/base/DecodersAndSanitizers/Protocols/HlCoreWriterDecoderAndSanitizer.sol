280a9a7faef448f92684790a9e4fc2f60190f5e4
alpha <agarwalpratham1812@gmail.com>
2025-11-10 16:29:07 +0530

    fix:typo

--- src/base/DecodersAndSanitizers/Protocols/HlCoreWriterDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/Protocols/HlCoreWriterDecoderAndSanitizer.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.21;
 
 import {BaseDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol";
 
-abstract contract CoreWriterDecoderAndSanitizer is BaseDecoderAndSanitizer {
+abstract contract HlCoreWriterDecoderAndSanitizer is BaseDecoderAndSanitizer {
     uint64 constant G = 10;
 
     error CoreWriterDecoderAndSanitizer__InvalidEncodingVersion();
