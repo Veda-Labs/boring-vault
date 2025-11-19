2a77b16f01e1989ab154634765ac0056d7388303
RadioJulius <9730701+RadioJulius@users.noreply.github.com>
2025-11-19 16:12:34 -0500

    deploy decoders and sanitizers for ink and mainnet and generate roots

--- script/DeployDecoderAndSanitizer.s.sol
+++ script/DeployDecoderAndSanitizer.s.sol
@@ -116,6 +116,9 @@ import {LiquidETHPlasmaDecoderAndSanitizer} from "src/base/DecodersAndSanitizers
 import {PlasmaUSDPlusPlasmaDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/PlasmaUSDPlusPlasmaDecoderAndSanitizer.sol";
 import {TurtleMUSDDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/TurtleMUSDDecoderAndSanitizer.sol";
 import {TestVault0DecoderAndSanitizer} from "src/base/DecodersAndSanitizers/TestVault0DecoderAndSanitizer.sol";
+import {SentoraUSDCInkDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/SentoraUSDCInkDecoderAndSanitizer.sol";
+import {SentoraUSDCMainnetDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/SentoraUSDCMainnetDecoderAndSanitizer.sol";
+import {ITBBasePositionDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/Protocols/ITB/ITBBasePositionDecoderAndSanitizer.sol";
 
 import "forge-std/Script.sol";
 import "forge-std/StdJson.sol";
@@ -137,8 +140,8 @@ contract DeployDecoderAndSanitizerScript is Script, ContractNames, MainnetAddres
     function setUp() external {
         privateKey = vm.envUint("BORING_DEVELOPER");
 
-        vm.createSelectFork("sepolia");
-        setSourceChainName("sepolia");
+        vm.createSelectFork("ink");
+        setSourceChainName("ink");
     }
 
     function run() external {
@@ -146,16 +149,11 @@ contract DeployDecoderAndSanitizerScript is Script, ContractNames, MainnetAddres
         bytes memory constructorArgs;
         vm.startBroadcast(privateKey);
 
-        creationCode = type(EtherFiLiquidUsdDecoderAndSanitizer).creationCode;
-        constructorArgs = abi.encode(getAddress(sourceChain, "uniswapV3NonFungiblePositionManager"), getAddress(sourceChain, "odosRouterV2"));
-        deployer.deployContract("Etherfi LiquidUSD Decoder And Sanitizer V0.7", creationCode, constructorArgs, 0);
-
-        creationCode = type(TurtleMUSDDecoderAndSanitizer).creationCode;
-        constructorArgs = abi.encode(getAddress(sourceChain, "odosRouterV2"), getAddress(sourceChain, "pancakeSwapV3NonFungiblePositionManager"), getAddress(sourceChain, "pancakeSwapV3MasterChefV3"));
+        creationCode = type(SentoraUSDCInkDecoderAndSanitizer).creationCode;
+        constructorArgs = abi.encode();
+        console.log("SentoraUSDC Ink Decoder and Sanitizer V0.0");
         console.logBytes(constructorArgs);
-        deployer.deployContract("Turtle MUSD Decoder and Sanitizer V0.1", creationCode, constructorArgs, 0);
-
-
+        deployer.deployContract("SentoraUSDC Ink Decoder and Sanitizer V0.0", creationCode, constructorArgs, 0);
         vm.stopBroadcast();
     }
 
