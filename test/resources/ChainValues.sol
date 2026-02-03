d4a00bac2aa95afb76bc9bef14c5bce44b0be3a9
Pratham Agarwal <agarwalpratham1812@gmail.com>
2026-02-04 03:30:50 +0530

    feat: updated configs

--- test/resources/ChainValues.sol
+++ test/resources/ChainValues.sol
@@ -1665,11 +1665,11 @@ contract ChainValues {
 
     function _addArbitrumValues() private {
         // Liquid Ecosystem
-        values[arbitrum]["deployerAddress"] = 0x771263e3Bc6aCDa5aE388A3F8A0c2dd7A17275FC.toBytes32();
+        values[arbitrum]["deployerAddress"] = 0x5BD97A73333B6EC2e38B687bcED159566A14C5BA.toBytes32();
         values[arbitrum]["dev0Address"] = 0x1b514df3413DA9931eB31f2Ab72e32c0A507Cad5.toBytes32();
         values[arbitrum]["dev1Address"] = 0x1b514df3413DA9931eB31f2Ab72e32c0A507Cad5.toBytes32();
         values[arbitrum]["liquidPayoutAddress"] = 0x1b514df3413DA9931eB31f2Ab72e32c0A507Cad5.toBytes32();
-        values[arbitrum]["txBundlerAddress"] = 0x771263e3Bc6aCDa5aE388A3F8A0c2dd7A17275FC.toBytes32();
+        values[arbitrum]["txBundlerAddress"] = 0x5BD97A73333B6EC2e38B687bcED159566A14C5BA.toBytes32();
 
         // DeFi Ecosystem
         values[arbitrum]["ETH"] = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE.toBytes32();
