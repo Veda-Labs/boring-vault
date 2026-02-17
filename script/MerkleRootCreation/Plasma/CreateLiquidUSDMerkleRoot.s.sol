715fd18079be9f035439d7b2c65d9d801fbadb57
dave alexander <dave.alexander@hey.com>
2026-02-17 11:27:48 -0700

    add yuzu leafs to mainnet liquidUSD

--- script/MerkleRootCreation/Plasma/CreateLiquidUSDMerkleRoot.s.sol
+++ script/MerkleRootCreation/Plasma/CreateLiquidUSDMerkleRoot.s.sol
@@ -69,6 +69,9 @@ contract CreateLiquidUSDMerkleRoot is Script, MerkleTreeHelper {
             getAddress(sourceChain, "dev1Address")
         );
 
+        // ========================== Yuzu ============================
+        _addYuzuLeafs(leafs);
+
         // ========================== Verify ==========================
         _verifyDecoderImplementsLeafsFunctionSelectors(leafs);
 
