258ecf82db24298ada86e6e4fddbb44021b71ac5
Samarpan <eth_sign77@proton.me>
2026-02-26 08:23:39 +0530

    feat: added lzReceive params

--- test/integrations/KatanaVbUsdcBridgeIntegration.t.sol
+++ test/integrations/KatanaVbUsdcBridgeIntegration.t.sol
@@ -159,7 +159,7 @@ contract KatanaOVaultIntegrationTest is Test, MerkleTreeHelper {
             to: bytes32(uint256(uint160(recipient))),
             amountLD: expectedAssets,
             minAmountLD: minAssets,
-            extraOptions: hex"",
+            extraOptions: addExecutorLzReceiveOption(abi.encodePacked(uint16(3)), 100_000, 0),
             composeMsg: hex"",
             oftCmd: hex""
         });
@@ -230,6 +230,7 @@ contract KatanaOVaultIntegrationTest is Test, MerkleTreeHelper {
 
     uint8 internal constant WORKER_ID = 1;
     uint16 internal constant TYPE_3 = 3;
+    uint8 internal constant OPTION_TYPE_LZRECEIVE = 1;
     uint8 internal constant OPTION_TYPE_LZCOMPOSE = 3;
 
     error InvalidOptionType(uint16 optionType);
@@ -266,8 +267,22 @@ contract KatanaOVaultIntegrationTest is Test, MerkleTreeHelper {
         return abi.encodePacked(_options, WORKER_ID, _option.length.toUint16() + 1, _optionType, _option);
     }
 
+    function encodeLzReceiveOption(uint128 _gas, uint128 _value) internal pure returns (bytes memory) {
+        return _value == 0 ? abi.encodePacked(_gas) : abi.encodePacked(_gas, _value);
+    }
+
     function encodeLzComposeOption(uint16 _index, uint128 _gas, uint128 _value) internal pure returns (bytes memory) {
         return _value == 0 ? abi.encodePacked(_index, _gas) : abi.encodePacked(_index, _gas, _value);
+    }
+
+    function addExecutorLzReceiveOption(bytes memory _options, uint128 _gas, uint128 _value)
+        internal
+        pure
+        onlyType3(_options)
+        returns (bytes memory)
+    {
+        bytes memory option = encodeLzReceiveOption(_gas, _value);
+        return addExecutorOption(_options, OPTION_TYPE_LZRECEIVE, option);
     }
 
     function addExecutorLzComposeOption(bytes memory _options, uint16 _index, uint128 _gas, uint128 _value)
