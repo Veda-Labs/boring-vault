993b05674bdfed611ff9e95eccd66e7217b6d596
Zokunei <zokunei@icloud.com>
2026-04-17 11:00:38 -0400

    audit concerns (comment unused var, add natspec, forge fmt)

--- src/base/DecodersAndSanitizers/Protocols/HyperLendDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/Protocols/HyperLendDecoderAndSanitizer.sol
@@ -10,7 +10,18 @@ import {AaveV3DecoderAndSanitizer} from "src/base/DecodersAndSanitizers/Protocol
 contract HyperLendDecoderAndSanitizer is AaveV3DecoderAndSanitizer {
     //============================== HyperLend Rewards Distributor ===============================
 
-    function claim(DecoderCustomTypes.Claim[] calldata claims) external pure virtual returns (bytes memory addressesFound) {
+    /**
+     *  @notice We intentionally do not report claims.token since it is not important for the security of the call.
+     *          HyperLend's merkle proofs bound what's claimable. Tokens are only transferred to msg.sender (the vault)
+     */
+    function claim(
+        DecoderCustomTypes.Claim[] calldata /*claims*/
+    )
+        external
+        pure
+        virtual
+        returns (bytes memory addressesFound)
+    {
         return addressesFound;
     }
 }
