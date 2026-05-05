1d2be5f9f0de872ced936fe1fea87d45979d2688
nitin gupta <nitin_g@me.iitr.ac.in>
2026-05-05 23:48:20 +0530

    feat: re-add RoycoDawn inheritance to composed decoder

--- src/base/DecodersAndSanitizers/RoycoJrUsdcDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/RoycoJrUsdcDecoderAndSanitizer.sol
@@ -4,5 +4,10 @@ pragma solidity 0.8.21;
 import {BaseDecoderAndSanitizer} from "./BaseDecoderAndSanitizer.sol";
 import {ERC4626DecoderAndSanitizer} from "./Protocols/ERC4626DecoderAndSanitizer.sol";
 import {SyrupDecoderAndSanitizer} from "./Protocols/SyrupDecoderAndSanitizer.sol";
+import {RoycoDawnDecoderAndSanitizer} from "./Protocols/RoycoDawnDecoderAndSanitizer.sol";
 
-contract RoycoJrUsdcDecoderAndSanitizer is ERC4626DecoderAndSanitizer, SyrupDecoderAndSanitizer {}
+contract RoycoJrUsdcDecoderAndSanitizer is
+    ERC4626DecoderAndSanitizer,
+    SyrupDecoderAndSanitizer,
+    RoycoDawnDecoderAndSanitizer
+{}
