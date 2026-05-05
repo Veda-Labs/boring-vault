bb79dc9bb51b1386e7d2acd61566250fc6802edd
nitin gupta <nitin_g@me.iitr.ac.in>
2026-05-05 19:30:34 +0530

    feat: simplify composed decoder to ERC4626 + Syrup only

--- src/base/DecodersAndSanitizers/RoycoJrUsdcDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/RoycoJrUsdcDecoderAndSanitizer.sol
@@ -4,10 +4,5 @@ pragma solidity 0.8.21;
 import {BaseDecoderAndSanitizer} from "./BaseDecoderAndSanitizer.sol";
 import {ERC4626DecoderAndSanitizer} from "./Protocols/ERC4626DecoderAndSanitizer.sol";
 import {SyrupDecoderAndSanitizer} from "./Protocols/SyrupDecoderAndSanitizer.sol";
-import {RoycoDawnDecoderAndSanitizer} from "./Protocols/RoycoDawnDecoderAndSanitizer.sol";
 
-contract RoycoJrUsdcDecoderAndSanitizer is
-    ERC4626DecoderAndSanitizer,
-    SyrupDecoderAndSanitizer,
-    RoycoDawnDecoderAndSanitizer
-{}
+contract RoycoJrUsdcDecoderAndSanitizer is ERC4626DecoderAndSanitizer, SyrupDecoderAndSanitizer {}
