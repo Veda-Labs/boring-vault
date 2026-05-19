b470d1603d727547a8a4ec9ebac2f0247d8850e9
Zokunei <zokunei@icloud.com>
2026-05-19 16:31:26 -0400

    add tx bundling for pausers

--- script/DeployMultiplePausers.s.sol
+++ script/DeployMultiplePausers.s.sol
@@ -16,40 +16,122 @@ import "forge-std/Script.sol";
 /**
  *
  * Usage (Trezor):
- *   forge script script/DeployMultiplePausers.s.sol:DeployMultiplePausers --rpc-url <RPC_URL> --trezor --broadcast --slow -vvvv
+ *   forge script script/DeployMultiplePausers.s.sol:DeployMultiplePausers --trezor --broadcast -vvvv
  */
 contract DeployMultiplePausers is Script, ChainValues, MainnetAddresses {
     string[] vaults = [
-        "eBTC",
-        "eUSD",
         "liquidBTC",
         "liquidETH",
         "liquidUSD",
-        "sETHFI",
-        "weETHk",
-        "weETHs"
+        "sETHFI"
+        // "weETHk",
+        // "weETHs"
     ];
 
+    string public sourceChain;
+
+    Deployer public deployer = Deployer(deployerAddress);
+
+    uint256 constant DESIRED_NUMBER_OF_DEPLOYMENT_TXS = 1;
+
+    Deployer.Tx[] internal txs;
+
     // ──────────────────────────────────────────────────────────────────────────
+
+    function setSourceChainName(string memory _chain) internal {
+        sourceChain = _chain;
+    }
+
+    function getTxs() public view returns (Deployer.Tx[] memory) {
+        return txs;
+    }
+
+    function _addTx(address target, bytes memory data, uint256 value) internal {
+        txs.push(Deployer.Tx(target, data, value));
+    }
+
+    function setUp() external {
+        setSourceChainName("optimism");
+        vm.createSelectFork(sourceChain);
+    }
 
     function run() external {
         bytes memory creationCode = type(Pauser).creationCode;
         bytes memory constructorArgs;
-        Deployer deployer = Deployer(deployerAddress);
 
         IPausable[] memory pausables = new IPausable[](0);
 
-        vm.startBroadcast();
-
         for (uint256 i = 0; i < vaults.length; i++) {
-            address vault = getAddress("mainnet", vaults[i]);
+            address vault = getAddress("mainnet", vaults[i]); // mainnet because address should be same across chains
             Authority rolesAuthority = Auth(vault).authority();
             string memory name = BoringVault(payable(vault)).name();
-            
+
+            string memory deploymentName = string.concat(name, " Pauser V0.2");
+            address _contract = deployer.getAddress(deploymentName);
+            if (_contract.code.length > 0) {
+                console.log(deploymentName, "already deployed at", _contract);
+                continue;
+            }
+
             constructorArgs = abi.encode(address(0), rolesAuthority, pausables);
-            deployer.deployContract(string.concat(name, " Pauser V0.2"), creationCode, constructorArgs, 0);
+            _addTx(
+                address(deployer),
+                abi.encodeWithSelector(deployer.deployContract.selector, deploymentName, creationCode, constructorArgs, 0),
+                0
+            );
+            console.log(string.concat(unicode"✅", deploymentName, " deployment to"), _contract, ": TX added");
         }
 
+        _bundleTxs();
+    }
+
+    function _bundleTxs() internal {
+        Deployer.Tx[] memory txsToSend = getTxs();
+        uint256 txsLength = txsToSend.length;
+
+        if (txsLength == 0) {
+            console.log("No txs to bundle");
+            return;
+        }
+
+        // Determine how many txs to send
+        uint256 desiredNumberOfDeploymentTxs = DESIRED_NUMBER_OF_DEPLOYMENT_TXS;
+        if (desiredNumberOfDeploymentTxs == 0) {
+            console.log("Desired number of deployment txs is 0");
+            return;
+        }
+        desiredNumberOfDeploymentTxs =
+            desiredNumberOfDeploymentTxs > txsLength ? txsLength : desiredNumberOfDeploymentTxs;
+        uint256 txsPerBundle = txsLength / desiredNumberOfDeploymentTxs;
+        uint256 lastIndexDeployed;
+        Deployer.Tx[][] memory txBundles = new Deployer.Tx[][](desiredNumberOfDeploymentTxs);
+
+        console.log(string.concat("Tx bundles to send: ", vm.toString(desiredNumberOfDeploymentTxs)));
+        console.log(string.concat("Total txs: ", vm.toString(txsLength)));
+
+        for (uint256 i; i < desiredNumberOfDeploymentTxs; i++) {
+            uint256 txsInBundle;
+            if (i == desiredNumberOfDeploymentTxs - 1) {
+                // Last bundle always collects all remaining transactions
+                txsInBundle = txsLength - lastIndexDeployed;
+            } else {
+                txsInBundle = txsPerBundle;
+            }
+            txBundles[i] = new Deployer.Tx[](txsInBundle);
+            for (uint256 j; j < txBundles[i].length; j++) {
+                txBundles[i][j] = txsToSend[lastIndexDeployed + j];
+            }
+            lastIndexDeployed += txsInBundle;
+        }
+
+        // Read tx bundler address from configuration file.
+        address txBundler = getAddress(sourceChain, "txBundlerAddress");
+
+        vm.startBroadcast();
+        for (uint256 i; i < desiredNumberOfDeploymentTxs; i++) {
+            console.log(string.concat("Sending bundle: ", vm.toString(i)));
+            Deployer(txBundler).bundleTxs(txBundles[i]);
+        }
         vm.stopBroadcast();
     }
 }
