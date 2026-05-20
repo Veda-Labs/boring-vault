b491141f64430aa00966901fac0eb24205fc464f
Eidolon <92181746+imrtlfarm@users.noreply.github.com>
2026-05-19 20:23:42 -0700

    modify to include vault v1 as well

--- src/helper/MorphoV2ERC4626BufferLens.sol
+++ src/helper/MorphoV2ERC4626BufferLens.sol
@@ -10,16 +10,23 @@ import {IERC4626} from "forge-std/interfaces/IERC4626.sol";
 import {TellerWithBuffer} from "src/base/Roles/TellerWithBuffer.sol";
 import {ERC4626BufferHelper, IBufferHelper} from "src/base/Roles/ERC4626BufferHelper.sol";
 import {IBufferLens} from "src/interfaces/IBufferLens.sol";
-import {IMorpho, Id, Market} from "src/interfaces/IMorpho.sol";
+import {IMorpho, Id, Market, MarketParams} from "src/interfaces/IMorpho.sol";
 
 interface IMorphoV2Vault is IERC4626 {
     function liquidityAdapter() external view returns (address);
     function liquidityData() external view returns (bytes memory);
 }
 
-interface ILiquidityAdapter {
+interface IMorphoMarketV1LiquidityAdapter {
     function morpho() external view returns (IMorpho);
+    function adaptiveCurveIrm() external view returns (address);
+    function allocation(MarketParams memory) external view returns (uint256);
     function expectedSupplyAssets(bytes32) external view returns (uint256);
+}
+
+interface IMorphoVaultV1LiquidityAdapter {
+    function morphoVaultV1() external view returns (address);
+    function allocation() external view returns (uint256);
 }
 
 contract MorphoV2ERC4626BufferLens is IBufferLens {
@@ -43,11 +50,11 @@ contract MorphoV2ERC4626BufferLens is IBufferLens {
             // Withdrawable is at least idle capital in the ERC4626 vault.
             withdrawableAmount = asset.balanceOf(address(erc4626Vault));
 
-            // Get the currently configured liquidity adapter market.
+            // Get the currently configured liquidity adapter sleeve.
             IMorphoV2Vault morphoV2Vault = IMorphoV2Vault(address(erc4626Vault));
             address liquidityAdapterAddress = morphoV2Vault.liquidityAdapter();
             if (liquidityAdapterAddress != address(0)) {
-                withdrawableAmount += _getConfiguredMarketLiquidity(
+                withdrawableAmount += _getConfiguredLiquidity(
                     asset, liquidityAdapterAddress, morphoV2Vault.liquidityData()
                 );
             }
@@ -59,6 +66,36 @@ contract MorphoV2ERC4626BufferLens is IBufferLens {
         }
     }
 
+    function _getConfiguredLiquidity(ERC20 asset, address liquidityAdapterAddress, bytes memory liquidityData)
+        internal
+        view
+        returns (uint256)
+    {
+        IMorphoVaultV1LiquidityAdapter morphoVaultV1Adapter = IMorphoVaultV1LiquidityAdapter(liquidityAdapterAddress);
+        try morphoVaultV1Adapter.morphoVaultV1() returns (address morphoVaultV1) {
+            return _getConfiguredVaultV1Liquidity(
+                asset, morphoVaultV1Adapter, IERC4626(morphoVaultV1), liquidityAdapterAddress, liquidityData
+            );
+        } catch {
+            return _getConfiguredMarketLiquidity(asset, liquidityAdapterAddress, liquidityData);
+        }
+    }
+
+    function _getConfiguredVaultV1Liquidity(
+        ERC20 asset,
+        IMorphoVaultV1LiquidityAdapter liquidityAdapter,
+        IERC4626 morphoVaultV1,
+        address liquidityAdapterAddress,
+        bytes memory liquidityData
+    ) internal view returns (uint256) {
+        if (liquidityData.length != 0) return 0;
+        if (liquidityAdapter.allocation() == 0) return 0;
+        if (address(morphoVaultV1) == address(0)) return 0;
+        if (morphoVaultV1.asset() != address(asset)) return 0;
+
+        return morphoVaultV1.maxWithdraw(liquidityAdapterAddress);
+    }
+
     function _getConfiguredMarketLiquidity(ERC20 asset, address liquidityAdapterAddress, bytes memory liquidityData)
         internal
         view
@@ -66,9 +103,16 @@ contract MorphoV2ERC4626BufferLens is IBufferLens {
     {
         if (liquidityData.length == 0) return 0;
 
-        ILiquidityAdapter liquidityAdapter = ILiquidityAdapter(liquidityAdapterAddress);
+        if (liquidityData.length != 160) return 0;
+
+        MarketParams memory marketParams = abi.decode(liquidityData, (MarketParams));
+        IMorphoMarketV1LiquidityAdapter liquidityAdapter = IMorphoMarketV1LiquidityAdapter(liquidityAdapterAddress);
+        if (marketParams.loanToken != address(asset)) return 0;
+        if (marketParams.irm != liquidityAdapter.adaptiveCurveIrm()) return 0;
+        if (liquidityAdapter.allocation(marketParams) == 0) return 0;
+
         IMorpho morpho = liquidityAdapter.morpho();
-        bytes32 marketId = keccak256(liquidityData);
+        bytes32 marketId = keccak256(abi.encode(marketParams));
         Market memory market = morpho.market(Id.wrap(marketId));
 
         if (market.totalSupplyAssets <= market.totalBorrowAssets) return 0;
--- test/MorphoV2ERC4626BufferLens.t.sol
+++ test/MorphoV2ERC4626BufferLens.t.sol
@@ -94,14 +94,60 @@ contract MockMorpho {
 
 contract MockLiquidityAdapter {
     IMorpho public morpho;
+    address public adaptiveCurveIrm;
     mapping(bytes32 => uint256) public expectedSupplyAssets;
+    mapping(bytes32 => uint256) internal allocations;
 
-    constructor(IMorpho morpho_) {
+    constructor(IMorpho morpho_, address adaptiveCurveIrm_) {
         morpho = morpho_;
+        adaptiveCurveIrm = adaptiveCurveIrm_;
+    }
+
+    function setAdaptiveCurveIrm(address adaptiveCurveIrm_) external {
+        adaptiveCurveIrm = adaptiveCurveIrm_;
     }
 
     function setExpectedSupplyAssets(bytes32 marketId, uint256 adapterSupplyAssets) external {
         expectedSupplyAssets[marketId] = adapterSupplyAssets;
+    }
+
+    function setAllocation(MarketParams memory marketParams, uint256 allocationAssets) external {
+        allocations[keccak256(abi.encode(marketParams))] = allocationAssets;
+    }
+
+    function allocation(MarketParams memory marketParams) external view returns (uint256) {
+        return allocations[keccak256(abi.encode(marketParams))];
+    }
+}
+
+contract MockMorphoVaultV1 is ERC4626 {
+    uint256 internal maxWithdraw_;
+
+    constructor(ERC20 asset_) ERC4626(asset_, "Mock MorphoV1 Vault", "mmv1") {}
+
+    function setMaxWithdraw(uint256 assets) external {
+        maxWithdraw_ = assets;
+    }
+
+    function maxWithdraw(address) public view override returns (uint256) {
+        return maxWithdraw_;
+    }
+
+    function totalAssets() public pure override returns (uint256) {
+        return 0;
+    }
+}
+
+contract MockMorphoVaultV1LiquidityAdapter {
+    address public morphoVaultV1;
+    uint256 public allocation;
+
+    constructor(address morphoVaultV1_) {
+        morphoVaultV1 = morphoVaultV1_;
+    }
+
+    function setAllocation(uint256 assets) external {
+        allocation = assets;
     }
 }
 
@@ -109,6 +155,7 @@ contract MorphoV2ERC4626BufferLensTest is Test {
     address internal boringVault = address(0xB0B);
 
     MockERC20Mintable internal asset;
+    address internal adaptiveCurveIrm = address(0x1A11);
     MockMorphoV2Vault internal morphoV2Vault;
     MockERC4626BufferHelper internal bufferHelper;
     MockTellerWithBufferShape internal teller;
@@ -119,7 +166,7 @@ contract MorphoV2ERC4626BufferLensTest is Test {
     function setUp() external {
         asset = new MockERC20Mintable();
         morpho = new MockMorpho();
-        liquidityAdapter = new MockLiquidityAdapter(IMorpho(address(morpho)));
+        liquidityAdapter = new MockLiquidityAdapter(IMorpho(address(morpho)), adaptiveCurveIrm);
         morphoV2Vault = new MockMorphoV2Vault(asset);
         morphoV2Vault.setLiquidityAdapter(address(liquidityAdapter));
         bufferHelper = new MockERC4626BufferHelper(ERC4626(address(morphoV2Vault)));
@@ -202,6 +249,93 @@ contract MorphoV2ERC4626BufferLensTest is Test {
         assertEq(_withdrawable(), 300e18);
     }
 
+    function testReturnsIdleOnlyForConfiguredMarketWithWrongLoanToken() external {
+        MockERC20Mintable otherAsset = new MockERC20Mintable();
+
+        _setBoringVaultShareClaim(1_000e18);
+        asset.mint(address(morphoV2Vault), 100e18);
+        asset.mint(address(morpho), 1_000e18);
+
+        bytes32 marketId = _setLiquidityData(
+            MarketParams(address(otherAsset), address(0xC011), address(0x0A11), adaptiveCurveIrm, 0.86e18)
+        );
+        liquidityAdapter.setExpectedSupplyAssets(marketId, 500e18);
+        morpho.setMarket(marketId, 1_000e18, 0);
+
+        assertEq(_withdrawable(), 100e18);
+    }
+
+    function testReturnsIdleOnlyForConfiguredMarketWithWrongIrm() external {
+        _setBoringVaultShareClaim(1_000e18);
+        asset.mint(address(morphoV2Vault), 100e18);
+        asset.mint(address(morpho), 1_000e18);
+
+        bytes32 marketId =
+            _setLiquidityData(MarketParams(address(asset), address(0xC011), address(0x0A11), address(0xBAD), 0.86e18));
+        liquidityAdapter.setExpectedSupplyAssets(marketId, 500e18);
+        morpho.setMarket(marketId, 1_000e18, 0);
+
+        assertEq(_withdrawable(), 100e18);
+    }
+
+    function testReturnsIdleOnlyForConfiguredMarketWithZeroAllocation() external {
+        _setBoringVaultShareClaim(1_000e18);
+        asset.mint(address(morphoV2Vault), 100e18);
+        asset.mint(address(morpho), 1_000e18);
+
+        MarketParams memory marketParams =
+            MarketParams(address(asset), address(0xC011), address(0x0A11), adaptiveCurveIrm, 0.86e18);
+        bytes32 marketId = _setLiquidityData(marketParams);
+        liquidityAdapter.setAllocation(marketParams, 0);
+        liquidityAdapter.setExpectedSupplyAssets(marketId, 500e18);
+        morpho.setMarket(marketId, 1_000e18, 0);
+
+        assertEq(_withdrawable(), 100e18);
+    }
+
+    function testUsesConfiguredMorphoVaultV1AdapterMaxWithdraw() external {
+        _setBoringVaultShareClaim(1_000e18);
+        asset.mint(address(morphoV2Vault), 100e18);
+        _setMorphoVaultV1LiquidityAdapter(asset, 300e18, 240e18);
+
+        assertEq(_withdrawable(), 340e18);
+    }
+
+    function testCapsConfiguredMorphoVaultV1AdapterByBoringVaultShareClaim() external {
+        _setBoringVaultShareClaim(300e18);
+        asset.mint(address(morphoV2Vault), 100e18);
+        _setMorphoVaultV1LiquidityAdapter(asset, 500e18, 500e18);
+
+        assertEq(_withdrawable(), 300e18);
+    }
+
+    function testReturnsIdleOnlyForMorphoVaultV1AdapterWithZeroAllocation() external {
+        _setBoringVaultShareClaim(1_000e18);
+        asset.mint(address(morphoV2Vault), 100e18);
+        _setMorphoVaultV1LiquidityAdapter(asset, 0, 240e18);
+
+        assertEq(_withdrawable(), 100e18);
+    }
+
+    function testReturnsIdleOnlyForMorphoVaultV1AdapterWithNonEmptyLiquidityData() external {
+        _setBoringVaultShareClaim(1_000e18);
+        asset.mint(address(morphoV2Vault), 100e18);
+        _setMorphoVaultV1LiquidityAdapter(asset, 300e18, 240e18);
+        morphoV2Vault.setLiquidityData(hex"1234");
+
+        assertEq(_withdrawable(), 100e18);
+    }
+
+    function testReturnsIdleOnlyForMorphoVaultV1AdapterAssetMismatch() external {
+        MockERC20Mintable otherAsset = new MockERC20Mintable();
+
+        _setBoringVaultShareClaim(1_000e18);
+        asset.mint(address(morphoV2Vault), 100e18);
+        _setMorphoVaultV1LiquidityAdapter(otherAsset, 300e18, 240e18);
+
+        assertEq(_withdrawable(), 100e18);
+    }
+
     function _setBoringVaultShareClaim(uint256 assets) internal {
         morphoV2Vault.mintShares(boringVault, assets);
         morphoV2Vault.setTotalAssets(assets);
@@ -213,7 +347,22 @@ contract MorphoV2ERC4626BufferLensTest is Test {
 
     function _setLiquidityData(MarketParams memory marketParams) internal returns (bytes32 marketId) {
         marketId = _marketId(marketParams);
+        liquidityAdapter.setAllocation(marketParams, 1);
         morphoV2Vault.setLiquidityData(abi.encode(marketParams));
+    }
+
+    function _setMorphoVaultV1LiquidityAdapter(ERC20 vaultAsset, uint256 allocation, uint256 maxWithdrawAmount)
+        internal
+    {
+        MockMorphoVaultV1 morphoVaultV1 = new MockMorphoVaultV1(vaultAsset);
+        morphoVaultV1.setMaxWithdraw(maxWithdrawAmount);
+
+        MockMorphoVaultV1LiquidityAdapter morphoVaultV1Adapter =
+            new MockMorphoVaultV1LiquidityAdapter(address(morphoVaultV1));
+        morphoVaultV1Adapter.setAllocation(allocation);
+
+        morphoV2Vault.setLiquidityAdapter(address(morphoVaultV1Adapter));
+        morphoV2Vault.setLiquidityData(hex"");
     }
 
     function _marketId(MarketParams memory marketParams) internal pure returns (bytes32) {
